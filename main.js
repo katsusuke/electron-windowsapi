974258a55d93f31212682404b45c2c65fc0144dc
Katsusuke Shimizu <katsusuke@gmail.com>
2016-05-31 13:02:37 +0900

    Hello Word with MessageBox API

--- main.js
+++ main.js
@@ -3,6 +3,17 @@ const {app} = electron;
 const {BrowserWindow} = electron;
 let win;
 
+var FFI = require('ffi');
+function TEXT(text){
+   return new Buffer(text, 'ucs2').toString('binary');
+}
+
+var user32 = new FFI.Library('user32', {
+   'MessageBoxW': [
+      'int32', [ 'int32', 'string', 'string', 'int32' ]
+   ]
+});
+
 function createWindow() {
   win = new BrowserWindow({width: 800, height: 600});
   win.loadURL(`file://${__dirname}/index.html`);
@@ -10,6 +21,9 @@ function createWindow() {
   win.on('closed', () => {
     win = null;
   });
+  var OK_or_Cancel = user32.MessageBoxW(
+     0, TEXT('I am Node.JS!'), TEXT('Hello, World!'), 1
+  );
 }
 
 app.on('ready', createWindow);
